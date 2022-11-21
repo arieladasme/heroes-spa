ee1f3061bd81bcc1c4ac7cbbc077f7a8ca2f4d00
ariel <ariel.wm1@gmail.com>
2022-11-21 17:28:16 -0300

    messages

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -10,11 +10,14 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse(location.search)
   const heroes = getHeroesByName(q)
 
+  const showSearch = q.length === 0
+  const showError = q.length > 0 && heroes.length === 0
+
   const { searchText, onInputChange } = useForm({ searchText: q })
 
   const onSearchSubmit = (e) => {
     e.preventDefault() //evita fullrefresh
-    if (searchText.trim().length <= 1) return
+    //if (searchText.trim().length <= 1) return
     navigate(`?q=${searchText}`)
   }
 
@@ -43,8 +46,26 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Result</h4>
           <hr />
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">
+          {/*   {q === '' ? (
+            <div className="alert alert-primary">Search a hero</div>
+          ) : (
+            heroes.length === 0 && (
+              <div className="alert alert-danger">
+                No hero with <b>{q}</b>
+              </div>
+            )
+          )} */}
+
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}
+          >
+            Search a hero
+          </div>
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}
+          >
             No hero with <b>{q}</b>
           </div>
           {heroes.map((hero) => (
